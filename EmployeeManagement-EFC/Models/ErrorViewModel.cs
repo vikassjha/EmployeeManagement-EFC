92c81cff97ba768bb3465e059d85f9be75d43a92
vikassjha <imvj0509@gmail.com>
2024-02-22 17:31:41 +0530

    Add project files.

---
+++ EmployeeManagement-EFC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeManagement_EFC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
